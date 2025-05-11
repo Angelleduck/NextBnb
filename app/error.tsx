9b7c887a4649b9c0fef7fd86af097c4c540c0951
Alex <angelleduck@yahoo.fr>
2025-05-11 11:15:11 +0000

    minor changes added

---
+++ app/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import EmptyState from "./_components/EmptyState";
+
+export default function Error() {
+  return <EmptyState title="Uh Oh" subtitle="Something went wrong!" />;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import EmptyState from "./_components/EmptyState";
+
+export default function NotFound() {
+  return <EmptyState title="Uh Oh" subtitle="Something went wrong!" />;
+}
